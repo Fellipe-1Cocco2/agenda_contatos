e4c575a28f4a064948b5959c3f41211674808da4
Fellipe Cocco <156376968+Fellipe-1Cocco2@users.noreply.github.com>
2025-03-28 02:33:10 -0300

    Sprint 2
    telas prontas no html e css, pronto para começar o JS
    aplicação loga, mas bem crua ainda

--- scripts/rotas.js
+++ scripts/rotas.js
@@ -15,4 +15,19 @@ export function definirRotas(app) {
   app.get("/login", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/pages/login.html"));
   });
+
+  // Rota favoritos
+  app.get("/favoritos", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/pages/favoritos.html"));
+  });
+
+  // Rota lixeira
+  app.get("/lixeira", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/pages/lixeira.html"));
+  });
+
+  // Rota Criar Contato
+  app.get("/criar-contato", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/pages/criar_contato.html"));
+  });
 }
